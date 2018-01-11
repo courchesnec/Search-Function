d3c30971e49ad6273c78cf1320759ec35fadd7cd
Colin Courchesne <colin.courchesne55@gmail.com>
2018-01-11 16:36:56 -0500

    Fetch

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import ItemSearch from 'amazon-product-api';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import { awsmobile } from './aws-exports';
 import SearchBar from './Components/SearchBar';
 import AppHeader from './Components/AppHeader';
 import ItemList from './Components/ItemList';
 
 const AMZ_API_KEY = 'AKIAJTJQAZ6XUQFB6NSQ';
 const AMZ_ASS_TAG = 'xxxxxxxxxxxxxxxxxxx';
+const WAL_API_KEY = 'xxxxxxxxxxxxxxxxxxx';
 
 
 export default class App extends Component {
@@ -14,19 +17,24 @@ export default class App extends Component {
     loading: false,
     items: []
   }
+  fetch('http://webservices.amazon.com/onca/xml?', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    Service=AWSECommerceService&,
+    AWSAccessKeyId=AMZ_API_KEY&,
+    AssociateTag=AMZ_ASS_TAG&,  
+    Operation=ItemSearch&,
+    Brand=&,
+    Availability=Available&,
+    SearchIndex=&,
+    Keywords=keyword
+  }),
+});
 
-  onPressSearch = keyword => {
-    this.searchAmazonWalmart(keyword);
-  }
-
-  searchAmazonWalmart = keyword => {
-    this.setState({ loading: true });
-    ItemSearch({ key: AMZ_API_KEY, AMZ_ASS_TAG, keyword }, items => {
-      this.setState({
-        loading: false,
-        items
-      });
-    });
   }
 
   render() {
